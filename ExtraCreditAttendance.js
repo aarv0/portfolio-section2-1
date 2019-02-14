87ef51f81424e1468bf9f5a4fd285e5937d75c75
Thor Anderson <ta.anderson@gmail.com>
2019-02-14 11:33:42 -0700

    Students who were here on time Feb 14 2019

--- ExtraCreditAttendance.js
+++ ExtraCreditAttendance.js
@@ -1,3 +1,7 @@
 const students_earning_extra_credit_attendance = [
-    Kristin, Bryon, Nate, David, Ben, Logan, Britten
+    'Kristin', 'Bryon', 'Nate', 'David', 'Ben', 'Logan', 'Britten'
+]
+
+const students_Feb14 = [
+    'Britten', 'Bryon', 'Patrick', 'Nate', 'Wyatt', 'Ben', 'Max'
 ]
