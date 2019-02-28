4b006c50ecc2dff4dd5ea07eca31b32fc637dfc5
Thor Anderson <ta.anderson@gmail.com>
2019-02-28 12:25:58 -0700

    Basic person grid laid out

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import { films } from './data/films.js/index.js'
+import { films } from './data/films.js'
 import { planets } from '../data/planets.js'
 
 films.sort((a, b) => (a.episode_id > b.episode_id ? 1 : -1))
