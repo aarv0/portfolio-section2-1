45f485ef95a24f24ec00254ab0fb3a85db19ac35
Thor Anderson <ta.anderson@gmail.com>
2019-03-07 07:03:29 -0700

    Got images loaded

--- scripts/swpeople.js
+++ scripts/swpeople.js
@@ -1,32 +1,62 @@
 import { people } from '../data/people.js'
+import { planets } from '../data/planets.js'
 
 const men = people.filter(person => person.gender === 'male')
 const women = people.filter(person => person.gender === 'female')
-const other = people.filter(person => (person.gender === 'n/a') || (person.gender === 'hermaphrodite') || (person.gender === 'none'))
+const other = people.filter(
+  person =>
+    person.gender === 'n/a' ||
+    person.gender === 'hermaphrodite' ||
+    person.gender === 'none',
+)
 
-//console.log(men, women, other)
+const getLastNumber = url => {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if(url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
+
+//https://starwars-visualguide.com/assets/img/characters/1.jpg
+
+const allHomeWorlds = people.map(person => {
+  let foundWorld = planets.find(element => {
+    return element.url === person.homeworld
+  })
+  let imageURL = getLastNumber(person.url)
+  
+  return {
+    name: person.name,
+    eye_color: person.eye_color,
+    home: foundWorld.name,
+    imagePath: `https://starwars-visualguide.com/assets/img/characters/${imageURL}.jpg`
+  }
+})
+
+console.log(allHomeWorlds)
 
 const mainContainer = document.createElement('div')
 mainContainer.className = 'container'
 
-men.forEach((man) => {
-    let manElement = document.createElement('div')
-    manElement.className = 'box'
-    manElement.textContent = man.name
-    let eyeColor = document.createElement('p')
-    eyeColor.textContent = man.eye_color
-    manElement.appendChild(eyeColor)
-    mainContainer.appendChild(manElement)
-})
+allHomeWorlds.forEach(person => {
+  let personElement = document.createElement('div')
+  let swatchElement = document.createElement('div')
+  let planetElement = document.createElement('p')
+  let imageElement = document.createElement('img')
 
-women.forEach((man) => {
-    let manElement = document.createElement('div')
-    manElement.className = 'box'
-    manElement.textContent = man.name
-    let eyeColor = document.createElement('p')
-    eyeColor.textContent = man.eye_color
-    manElement.appendChild(eyeColor)
-    mainContainer.appendChild(manElement)
+  personElement.className = 'card'
+  personElement.textContent = person.name
+  swatchElement.className = 'swatch mdi mdi-eye'
+  swatchElement.style.borderLeftColor = person.eye_color
+  planetElement.textContent = person.home
+  imageElement.src = person.imagePath
+
+  personElement.appendChild(planetElement)
+  personElement.appendChild(swatchElement)
+  personElement.appendChild(imageElement)
+  mainContainer.appendChild(personElement)
 })
 
 document.body.appendChild(mainContainer)
